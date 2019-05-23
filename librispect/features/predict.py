8975f7e3ca4137f6cd2b64c03265cc62d0a947ab
kachiem <36138540+kachiem@users.noreply.github.com>
2019-05-23 10:17:19 -0700

    numpy (#5)
    * numpy
    
    * spect_predict_maker testing
    
    * added documentation
    
    * fix import
    
    * fix path list
    
    * inherit spect_maker, fix infinite while loop
    
    * merge fix
    
    * fix utils import
    
    * attempt #1 to get batch size
    
    * remove while loop in predict batch_iter, try different object
    
    * swap pred_spect for test obj
    
    * clean up merge conflict headers

--- librispect/features/predict.py
+++ librispect/features/predict.py
@@ -1,7 +1,23 @@
+import numpy as np
 from librispect.features.spectrogram import slicing_window, spect_maker
+from librispect.utils import split_validation
 
 
-class spect_predict_maker:
+class spect_predict_maker(spect_maker):
+    '''
+    The Class spect_predict_maker contains a helper function and
+     a generator that returns batches of (term, p_term, labels) tuples
+     to feed into cpc_model.
+
+    Functions:
+        __init__: a initializer of class, pass in hparams, specify terms,
+                step and window size, and initialize spect_maker
+        batch_per_epoch: checks if batch size can be evenly split for one epoch
+        batch_iter: return a generator, takes in path_list and returns
+                  the converted numpy array from wav
+
+    '''
+
     def __init__(self, hparams, terms=4, predict_terms=4, step_size=1):
         self.hparams = hparams
         self.terms = terms
@@ -13,39 +29,46 @@ class spect_predict_maker:
         )
 
     def batch_per_epoch(self, path_list, batch_size):
+        ''' Check for even batch size in every epoch. '''
+
         assert batch_size % 2 == 0, "half positive, half negative examples"
         return self.spect_maker.batch_ss_per_epoch(path_list, batch_size / 2)
 
     def batch_iter(self, path_list, batch_size):
-        while True:
-            for spect, _ in self.spect_maker.spect_iter(path_list):
-                spect_sliced = slicing_window(spect, self.window_size, self.step_size)
-                x_terms = spect_sliced[..., : self.terms]
-                x_pterms = spect_sliced[..., -self.predict_terms :]
-                neg_terms = np.random.permutation(x_terms)
-                neg_pterms = np.random.permutation(x_pterms)
+        ''' 
+        Creates positive and negative examples for one batch.
+        Yields term_batch, p_term batch, and labels.
+        '''
 
-                num_batches = np.ceil(len(spect_sliced) / (batch_size / 2))
-                for x_term_batch, x_pterm_batch, neg_term_batch, neg_pterm_batch in zip(
-                    *[
-                        np.array_split(sliced, num_batches)
-                        for sliced in [x_terms, x_pterms, neg_terms, neg_pterms]
-                    ]
-                ):
+        for spect, _ in self.spect_maker.spect_iter(path_list):
+            spect_sliced = slicing_window(
+                spect, self.window_size, self.step_size)
+            x_terms = spect_sliced[..., : self.terms]
+            x_pterms = spect_sliced[..., -self.predict_terms:]
+            neg_terms = np.random.permutation(x_terms)
+            neg_pterms = np.random.permutation(x_pterms)
+
+            num_batches = np.ceil(len(spect_sliced) / (batch_size / 2))
+            for x_term_batch, x_pterm_batch, neg_term_batch, neg_pterm_batch in zip(
+                *[
+                    np.array_split(sliced, num_batches)
+                    for sliced in [x_terms, x_pterms, neg_terms, neg_pterms]
+                ]
+            ):
                     # keras likes time to be dimension 1
-                    term_batch = np.transpose(
-                        np.concatenate((x_term_batch, neg_term_batch)), (0, 2, 1)
+                term_batch = np.transpose(
+                    np.concatenate((x_term_batch, neg_term_batch)), (0, 2, 1)
+                )
+                pterm_batch = np.transpose(
+                    np.concatenate((x_pterm_batch, neg_pterm_batch)), (0, 2, 1)
+                )
+                labels = np.concatenate(
+                    (
+                        np.ones(x_term_batch.shape[0]),
+                        np.zeros(neg_term_batch.shape[0]),
                     )
-                    pterm_batch = np.transpose(
-                        np.concatenate((x_pterm_batch, neg_pterm_batch)), (0, 2, 1)
-                    )
-                    labels = np.concatenate(
-                        (
-                            np.ones(x_term_batch.shape[0]),
-                            np.zeros(neg_term_batch.shape[0]),
-                        )
-                    )
-                    idxs = np.random.permutation(labels.shape[0])
-                    yield [term_batch[idxs, ...], pterm_batch[idxs, ...]], labels[
-                        idxs, ...
-                    ]
+                )
+                idxs = np.random.permutation(labels.shape[0])
+                yield [term_batch[idxs, ...], pterm_batch[idxs, ...]], labels[
+                    idxs, ...
+                ]
---
+++ testing/test_predict.py
@@ -0,0 +1,40 @@
+import librispect as lspct
+from librispect.features import spectrogram, predict
+import glob
+import librosa
+import numpy as np
+import pytest
+from librispect.utils import split_validation
+
+SPECT_HEIGHT = 2048
+STFT_HEIGHT = 2048
+WINDOW_SIZE = 16
+BATCH_SIZE = 32
+STEP_SIZE = 4
+TERMS = 8
+PREDICT_TERMS = 8
+
+
+def test_spect_predict_maker():
+    '''integration testing of prediction generation'''
+    hparams = lspct.features.spectrogram.HPARAMS
+    test_spect_predict_maker = predict.spect_predict_maker(hparams, terms=TERMS, predict_terms=PREDICT_TERMS)
+    path_list = glob.glob((lspct.paths.WAV_DIR / '*.wav').as_posix())[0:1] 
+    print("path_list: ", path_list)
+    pred_spect = test_spect_predict_maker.batch_iter(path_list, 2)
+    #neg_batch, pos_batch, labels, idxs = test_spect_predict_maker.batch_iter(path_list, 2)
+    if len(path_list) <= 1:
+        # term_batch, pterm_batch, labels, idxs
+        print("test_spect_predict_maker: ", test_spect_predict_maker)
+        print("pred_spect: ", pred_spect)
+        batch_size = test_spect_predict_maker.batch_ss_per_epoch(path_list, 32)  # turn batch_set into param for epoch
+        # if (actual batch_per_epoch(batch_size) == expected batch_size)
+        if batch_size % 2 == 0:  # expected batch size:
+            train_path_list, val_path_list = split_validation(test_spect_predict_maker, 0.1)
+            return print("training: ", train_path_list, "\n", "validation: ", val_path_list)
+        else:
+            return "Uneven batch size"
+
+
+if __name__ == "__main__":
+    test_spect_predict_maker()
